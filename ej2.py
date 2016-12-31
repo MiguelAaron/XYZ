812e4e9be156554c34740e01353bbc22a3f55447
MiguelAaron <AaronXYZ.96@gmail.com>
2016-12-30 19:53:58 -0600

    verificar numeros primos

--- ej2.py
+++ ej2.py
@@ -1,12 +1,12 @@
-min = 200 #Escribir a partir de que numero se comienzara
+min = 200 #Escribir a partir de que numero se comenzara
 max = 300 #Escribir hasta que numero se terminara
 R = range(min, (max+1))
-s = 0 #+1 si la division tiene un cociente igual a 0
-c = 1 #
+s = 0 #+1 si la division tiene un residuo igual a 0
+c = 1 #Dividendos del numero a verificar
 
-for n in R:
+for n in R: #n es el numero a verificar
   
-  while c <= max:
+  while c <= max: #Dividendos, desde 1 hasta n
   
     if n%c == 0:
       s += 1
