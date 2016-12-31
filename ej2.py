7f7d4b594de79a27b9e64aa024891037a3d98f24
MiguelAaron <AaronXYZ.96@gmail.com>
2016-12-30 19:45:25 -0600

    Verificar numeros primos

--- ej2.py
+++ ej2.py
@@ -1,4 +1,20 @@
-F = "Hola mundo"
+min = 200 #Escribir a partir de que numero se comienzara
+max = 300 #Escribir hasta que numero se terminara
+R = range(min, (max+1))
+s = 0 #+1 si la division tiene un cociente igual a 0
+c = 1 #
 
-print(F)
-
+for n in R:
+  
+  while c <= max:
+  
+    if n%c == 0:
+      s += 1
+    
+    c += 1
+    
+  if s == 2:
+    print(n, "Es primo")
+  
+  s = 0
+  c = 1
